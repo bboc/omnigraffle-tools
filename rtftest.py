944afda7cea0b179f3a07a9b5768038c242e5d35
bboc <bernhard.bockelbrink@gmail.com>
2019-05-11 12:34:20 +0200

    added argparse, more extraction

--- rtftest.py
+++ rtftest.py
@@ -1,12 +1,27 @@
 # -*- coding: utf-8 -*-
 
 
-def extract_text_from_rtf(element):
+import argparse
+from lxml import etree as ET
+from rtf2md_tests import rtf2md
+
+
+def dump_element(element):
     raw_rtf = element[1].text
+    print('-' * 30)
+    try:
+        print(rtf2md(raw_rtf))
+    except:
+        print("error", raw_rtf)
+    # get to the parents parent (which is the Object itself)
+    parent = element.getparent()
+    for idx, item in enumerate(parent):
+        if item.text == "UserInfo":
+            print(idx, item.text)
 
 
 def print_unitest(element):
-    """extract marked up text from rtf."""
+    """Extract marked up text from rtf and output as a unittest skeleton."""
 
     raw_rtf = element[1].text
     print('''
@@ -22,31 +37,43 @@ def print_unitest(element):
     ''' % dict(raw_rtf=raw_rtf))
 
 
-if __name__ == "__main__":
+def cmd_extract(args):
+    print("extract - not implemented")
 
-    import xml.etree.ElementTree as ET
-    tree = ET.parse('test-data/test-data-file-package.graffle/data.plist')
+
+def cmd_inject(args):
+    print("inject - not implemented")
+
+
+def cmd_dump(args):
+    print("dump text")
+
+    tree = ET.parse(args.document)
     root = tree.getroot()
 
     for element in root.findall(".//dict//key/.."):
+        # get all dicts where the text of the first elementsi "Text"
+        # TODO: this is probably not very robust
         if element[0].text == "Text":
-            extract_text_from_rtf(element)
+            dump_element(element)
 
 
-"""
-/*/dict/text
+if __name__ == "__main__":
 
+    # create the top-level parser
+    parser = argparse.ArgumentParser(prog='ogtranslate')
+    parser.add_argument('document', help='the OmniGraffle document (or the plist inside it')
+    subparsers = parser.add_subparsers(help='sub-command help')
 
-<dict>
-                        <key>Text</key>
-                        <string>{\rtf1\ansi\ansicpg1252\cocoartf1561\cocoasubrtf600
-{\fonttbl\f0\fnil\fcharset0 HelveticaNeue;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\pard\tx560\tx1120\tx1680\tx2240\tx2800\tx3360\tx3920\tx4480\tx5040\tx5600\tx6160\tx6720\pardirnatural\qc\partightenfactor0
+    p_extract = subparsers.add_parser('extract', help='extract text from OmniGraffle document')
+    p_extract.add_argument('target', help='target file')
+    p_extract.set_defaults(func=cmd_extract)
+    p_inject = subparsers.add_parser('inject', help='inject text into OmniGraffle document')
+    p_inject.add_argument('text_source', help='text file')
+    p_inject.set_defaults(func=cmd_inject)
 
-\f0\fs32 \cf0 This is some 
-\b styled
-\b0  text}</string>
+    p_dump = subparsers.add_parser('dump', help='dump text into OmniGraffle document')
+    p_dump.set_defaults(func=cmd_dump)
 
-"""
+    args = parser.parse_args()
+    args.func(args)
