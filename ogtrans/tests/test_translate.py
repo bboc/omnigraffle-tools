84cfff7078522f46193a0a8422cf030c26861f7d
bboc <bernhard.bockelbrink@gmail.com>
2019-05-19 12:38:05 +0200

    better test setup for translate tests

--- ogtrans/tests/test_translate.py
+++ ogtrans/tests/test_translate.py
@@ -10,6 +10,7 @@ from ogtrans.commands import OmniGraffleTranslator
 class TranslateTests(unittest.TestCase):
 
     def setUp(self):
+        self.test_data = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'test-data')
         self.tmp_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'tmp')
         if not os.path.exists(self.tmp_path):
             os.makedirs(self.tmp_path)
@@ -19,17 +20,19 @@ class TranslateTests(unittest.TestCase):
         """
         One file with target pointing to one PO file should be correctly tranlsated.
         """
-        self.fail("not implemented")
         tmpfolder = tempfile.TemporaryDirectory(dir=self.tmp_path)
 
         args = Namespace(canvas=None,
                          func=OmniGraffleTranslator.cmd_translate,
                          loglevel=30,
-                         source='ogtrans/test-data/rtfprocessor-tests.graffle',
-                         target=tmpfolder.name)
+                         source=os.path.join(self.test_data, 'rtfprocessor-tests.graffle'),
+                         target=tmpfolder.name,
+                         translations=os.path.join(self.test_data, 'translations', 'rtfprocessor-tests.po'),)
 
         tr = OmniGraffleTranslator(args)
         tr.args.func(tr)
+
+        self.assertTrue(os.path.exists(os.path.join(tmpfolder.name, 'rtfprocessor-tests.graffle')))
 
     def test_po_and_markdown(self):
         """
